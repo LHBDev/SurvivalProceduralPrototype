9faa7aecaa38406da711e22ea79150c8a3ec881a
Nick K <nickk709@live.com>
2016-08-04 23:02:30 -0400

    Procedural Map Gen
    PRESS 0 to reset

---
+++ Assets/Scripts/BlockDestroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockDestroy : MonoBehaviour {
+   // BoxCollider testCollider;
+	// Use this for initialization
+	void Start () {
+        //testCollider = GetComponent<BoxCollider>();
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Collider>().tag == "Bullet")
+        {
+            DestroyObject(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
